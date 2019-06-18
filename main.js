54b841a901f1ae7c95281480198a8d9e0d9d4ff3
unknown <drgaglia@gmail.com>
2019-06-18 01:24:34 -0400

    added more

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let timeSignatureList = [];
 
-function play ( line ) 
+function playLine ( line ) 
 {
-    console.log ( line );
+    let audio = new Audio ( line );
+    audio.play ();   
 }
 
 function executePhase ( phase ) 
@@ -10,40 +11,62 @@ function executePhase ( phase )
     // Phase 1 has an intro
     if ( phase === 1 )
     {
-        play ( introLine );
-    }
-    
-    // Choose a random line for the current phase
-    let phaseLine = phaseLines [ phase - 1 ] [ Math.floor ( Math.random () * phaseLines [ phase - 1 ].length ) ];
-
-    play ( phaseLine );
-
-    if ( phase === 3 ) {
-        let timeSignatureLine1 = getTimeSignatureLine ();
-        let timeSignatureLine2 = getTimeSignatureLine ();
-        play ( nextNumberPhase3Line );
-        play ( timeSignatureLine1 );
-        play ( thenLine );
-        play ( timeSignatureLine2 );
-    }
-    else if ( phase === 4 )
-    {
-        // Could do some cool stuff at the end, this is here to be expanded upon later
-        console.log ( timeSignatureList );
-        document.getElementById ( "phase-4" ).disabled = true;
-        return;
+        playLine ( introLine );
+        setTimeout ( function () { 
+            executePhaseLines ( phase );           
+        }, 14000 );
     }
     else
     {
-        play ( nextNumberLine );
-        let timeSignatureLine = getTimeSignatureLine ();
-        play ( timeSignatureLine );
+        executePhaseLines ( phase );
     }
-  
-    // Disable the current phase's button, enable the next one
-    document.getElementById ( "phase-" + phase ).disabled = true;
-    document.getElementById ( "phase-" + ( phase + 1 ) ).disabled = false;
-    console.log ( "=================== END PHASE " + phase + " ===================");
+}
+
+function executePhaseLines ( phase ) 
+{
+    // Choose a random line for the current phase
+    let phaseLine = phaseLines [ phase - 1 ] [ Math.floor ( Math.random () * phaseLines [ phase - 1 ].length ) ];
+
+    playLine ( phaseLine );
+
+    setTimeout ( function () 
+    {
+        if ( phase === 3 )
+        {
+            let timeSignatureLine1 = getTimeSignatureLine ();
+            let timeSignatureLine2 = getTimeSignatureLine ();
+            playLine ( nextNumberPhase3Line );
+            setTimeout ( function () {
+                playLine ( timeSignatureLine1 );
+                setTimeout ( function () {
+                    playLine ( plusLine );
+                    setTimeout ( function () {                       
+                        playLine ( timeSignatureLine2 );
+                    }, 1000 );
+                }, 1000 );
+            }, 8500 );
+        }
+        else if ( phase === 4 )
+        {
+            // Could do some cool stuff at the end, this is here to be expanded upon later
+            console.log ( timeSignatureList );
+            document.getElementById ( "phase-4" ).disabled = true;
+            return;
+        }
+        else
+        {
+            playLine ( nextNumberLine );
+            let timeSignatureLine = getTimeSignatureLine ();
+            setTimeout ( function () {
+                playLine ( timeSignatureLine );
+            }, 8500);
+        }
+
+        // Disable the current phase's button, enable the next one
+        document.getElementById ( "phase-" + phase ).disabled = true;
+        document.getElementById ( "phase-" + ( phase + 1 ) ).disabled = false;
+        console.log ( "=================== END PHASE " + phase + " ===================");
+    }, 8000 );
 }
 
 function getTimeSignatureLine () 
@@ -79,32 +102,36 @@ let timeSignatureLines =
 const phaseLines = 
 [
     [
-        "phase_1_0.wav",
-        "phase_1_1.wav",
-        "phase_1_2.wav",
-        "phase_1_3.wav"
+        "phase_1_test.wav"
+        // "phase_1_0.wav",
+        // "phase_1_1.wav",
+        // "phase_1_2.wav",
+        // "phase_1_3.wav"
     ],
     [
-        "phase_2_0.wav",
-        "phase_2_1.wav",
-        "phase_2_2.wav",
-        "phase_2_3.wav",
+        "phase_2_test.wav"
+        // "phase_2_0.wav",
+        // "phase_2_1.wav",
+        // "phase_2_2.wav",
+        // "phase_2_3.wav",
     ],
     [
-        "phase_3_0.wav",
-        "phase_3_1.wav",
-        "phase_3_2.wav",
-        "phase_3_3.wav",
+        "phase_3_test.wav"
+        // "phase_3_0.wav",
+        // "phase_3_1.wav",
+        // "phase_3_2.wav",
+        // "phase_3_3.wav",
     ],
     [
-        "phase_end_0.wav",
-        "phase_end_1.wav",
-        "phase_end_2.wav",
-        "phase_end_3.wav",
+        "phase_4_test.wav"
+        // "phase_end_0.wav",
+        // "phase_end_1.wav",
+        // "phase_end_2.wav",
+        // "phase_end_3.wav",
     ]
 ];
 
 const introLine = "intro.wav";
-const thenLine = "then.wav";
+const plusLine = "plus.wav";
 const nextNumberLine = "next_number.wav";
 const nextNumberPhase3Line = "next_number_phase_3.wav";
